029c353818c312fee68638e1bf3cf9605d2ff092
Otengtech <otengebenezer326@gmail.com>
2025-05-28 18:46:47 -0700

    Commit

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import HealthyFoods from "./HealthyFoods";
 import WorkoutSection from "./WorkoutSection";
@@ -63,80 +63,78 @@ function App() {
 
   return (
     <div className="bg-white min-h-screen">
-      <BrowserRouter>
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HealthyFoods />
-                <ReviewSection />
-                <Footer />
-              </>
-            }
-          />
-          <Route path="/recipe" element={<RecipeSection />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/hydration" element={<Hydration />} />
-          <Route path="/superfoods" element={<Superfoods />} />
-          <Route path="/mindfulness" element={<Mindfulness />} />
-          <Route path="/sleep" element={<Sleep />} />
-          <Route path="/plantbased" element={<PlantBased />} />
-          <Route path="/strength" element={<Strength />} />
-          <Route path="/motivation" element={<Motivation />} />
-          <Route path="/morningworkout" element={<MorningWorkout />} />
-          <Route path="/workoutssection" element={<WorkoutSection />} />
-          <Route path="/aboutussection" element={<AboutUsSection />} />
-          <Route path="/mentalwellness" element={<MentalWellness />} />
-          <Route path="/energyfoods" element={<EnergyFoodGuide />} />
-          <Route path="/balancediet" element={<BalancedDiet />} />
-          <Route path="/labelread" element={<LabelRead />} />
-          <Route path="/qvb" element={<QuinoaVeggieBowl />} />
-          <Route path="/berrysmoothie" element={<BerrySmoothie />} />
-          <Route path="/csf" element={<ChickpeaStirFry />} />
-          <Route path="/zucchininoodles" element={<ZucchiniNoodles />} />
-          <Route path="/oatmeal" element={<OatmealWithFruits />} />
-          <Route path="/bsa" element={<BakedSalmonAsparagus />} />
-          <Route path="/avocadotoast" element={<AvocadoToast />} />
-          <Route path="/gcs" element={<GrilledChickenSalad />} />
-          <Route path="/gyp" element={<GreekYogurtParfait />} />
-          <Route path="/sbp" element={<StuffedBellPeppers />} />
-          <Route path="/lentilsoup" element={<LentilSoup />} />
-          <Route path="/evm" element={<EggVeggieMuffins />} />
-          <Route path="/supplements" element={<SupplementsGuidance />} />
-          <Route path="/mindfuleating" element={<MindfulEating />} />
-          <Route path="/hydrationeducation" element={<HydrationEducation />} />
-          <Route path="/sportsnutrition" element={<SportsNutrition />} />
-          <Route path="/foodsensitivities" element={<FoodSensitivities />} />
-          <Route path="/mealtiming" element={<MealTimingFrequency />} />
-          <Route path="/weightmanagement" element={<WeightManagement />} />
-          <Route
-            path="/nutritionCounseling"
-            element={<NutritionalCounseling />}
-          />
-          <Route path="/personaltraining" element={<PersonalTraining />} />
-          <Route path="/contactussection" element={<ContactUsSection />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route
-            path="*"
-            element={
-              <h1 className="text-center text-red-500 mt-10">
-                404 - Page Not Found
-              </h1>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <h1 className="text-center text-red-500 mt-10">
-                404 - Page Not Found
-              </h1>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HealthyFoods />
+              <ReviewSection />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/recipe" element={<RecipeSection />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/hydration" element={<Hydration />} />
+        <Route path="/superfoods" element={<Superfoods />} />
+        <Route path="/mindfulness" element={<Mindfulness />} />
+        <Route path="/sleep" element={<Sleep />} />
+        <Route path="/plantbased" element={<PlantBased />} />
+        <Route path="/strength" element={<Strength />} />
+        <Route path="/motivation" element={<Motivation />} />
+        <Route path="/morningworkout" element={<MorningWorkout />} />
+        <Route path="/workoutssection" element={<WorkoutSection />} />
+        <Route path="/aboutussection" element={<AboutUsSection />} />
+        <Route path="/mentalwellness" element={<MentalWellness />} />
+        <Route path="/energyfoods" element={<EnergyFoodGuide />} />
+        <Route path="/balancediet" element={<BalancedDiet />} />
+        <Route path="/labelread" element={<LabelRead />} />
+        <Route path="/qvb" element={<QuinoaVeggieBowl />} />
+        <Route path="/berrysmoothie" element={<BerrySmoothie />} />
+        <Route path="/csf" element={<ChickpeaStirFry />} />
+        <Route path="/zucchininoodles" element={<ZucchiniNoodles />} />
+        <Route path="/oatmeal" element={<OatmealWithFruits />} />
+        <Route path="/bsa" element={<BakedSalmonAsparagus />} />
+        <Route path="/avocadotoast" element={<AvocadoToast />} />
+        <Route path="/gcs" element={<GrilledChickenSalad />} />
+        <Route path="/gyp" element={<GreekYogurtParfait />} />
+        <Route path="/sbp" element={<StuffedBellPeppers />} />
+        <Route path="/lentilsoup" element={<LentilSoup />} />
+        <Route path="/evm" element={<EggVeggieMuffins />} />
+        <Route path="/supplements" element={<SupplementsGuidance />} />
+        <Route path="/mindfuleating" element={<MindfulEating />} />
+        <Route path="/hydrationeducation" element={<HydrationEducation />} />
+        <Route path="/sportsnutrition" element={<SportsNutrition />} />
+        <Route path="/foodsensitivities" element={<FoodSensitivities />} />
+        <Route path="/mealtiming" element={<MealTimingFrequency />} />
+        <Route path="/weightmanagement" element={<WeightManagement />} />
+        <Route
+          path="/nutritionCounseling"
+          element={<NutritionalCounseling />}
+        />
+        <Route path="/personaltraining" element={<PersonalTraining />} />
+        <Route path="/contactussection" element={<ContactUsSection />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route
+          path="*"
+          element={
+            <h1 className="text-center text-red-500 mt-10">
+              404 - Page Not Found
+            </h1>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <h1 className="text-center text-red-500 mt-10">
+              404 - Page Not Found
+            </h1>
+          }
+        />
+      </Routes>
     </div>
   );
 }
